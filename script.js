f6aac6d0a48d0e3fbb6554c43a5a20cd85253dd9
nihad <nihadamhine@gmail.com>
2024-11-03 00:08:38 +0100

    changment en bouton delete

--- script.js
+++ script.js
@@ -4,57 +4,6 @@ function affichercnt() {
 }
 
 
-// document.getElementById('task-form').addEventListener('submit',function(e){
-//     e.preventDefault();
-
-//     let title = document.getElementById('title').value;
-//     let description = document.getElementById('description').value;
-//     let date = document.getElementById('date').value;
-//     let status = document.getElementById('status').value;
-//     let type= document.getElementById('type').value;
-    
-//     if (title && description && date && status && type) {
-//     let taskItem = `<div class="col div bg-white">
-//                     <div class=" p-3 mt-3" id="contenu">
-//                         <h3> ${title}</h3>
-//                         <p> Description :${description}</p>
-//                         <p> Date :${date}</p>
-
-//                         <div >
-//                             <button type="button" class="btn btn-success  mt-2 ">Edit</button>
-//                             <button type="button" class=" btn btn-warning mt-2 id="delet-contenu" >Delete</button>    
-//                         </div>
-//                     </div> 
-//                     </div>`;
-
-   
-                    
-//     if (status === 'todo') {
-//         document.getElementById('todo').innerHTML += taskItem;
-//     } else if (status === 'doing') {
-//         document.getElementById('doing').innerHTML += taskItem;
-//     } else if (status === 'done') {
-//         document.getElementById('done').innerHTML += taskItem;
-//     }
-//     document.getElementById('task-form').reset();
-    
-    
-//     if (type === 'P1') {
-//         document.getElementById('P1').style.border = "2px solid red";    
-//     } else if (type === 'P2') {
-//         document.getElementById('P2').style.border = "2px solid orange"; 
-//     } else if (type === 'P3') {
-//         document.getElementById('P3').style.border = "2px solid green";  
-//     }
-   
-
-    
-    
-//     }else{
-//         alert("remplire les champt");
-//     }
-
-//     });
 
 
 document.getElementById('task-form').addEventListener('submit', function(e) {
@@ -101,10 +50,10 @@ document.getElementById('task-form').addEventListener('submit', function(e) {
         }
 
         
-        // let deleteButton = contenuDiv.querySelector('.delet-contenu');//selection delet-contenu pour suprission 
-        // deleteButton.addEventListener('click', function() {
-        //     taskItem.remove(); 
-        // });
+        let deleteButton = contenuDiv.querySelector('.delet-contenu');//selection delet-contenu pour suprission 
+        deleteButton.addEventListener('click', function() {
+            taskItem.remove(); 
+        });
 
     } else {
         alert("Veuillez remplir tous les champs");
@@ -120,16 +69,3 @@ document.getElementById('exit-button').addEventListener('click', function() {
 document.getElementById('add-button').addEventListener('click', function() {
     document.getElementById('task-form').style.visibility = 'visible'; 
 });
-
-// document.getElementById("delet-contenu").addEventListener('click', function () {
-
-//     let element = document.getElementById('contenu');
-//     if(element ) {
-//         element.remove();
-//     } else {
-//         alert("no element");
-//     }
-// });
-// document.getElementById('delet-contenu').addEventListener('click', function() {
-//     document.getElementById('contenu').style.visibility = 'hidden';
-// });
